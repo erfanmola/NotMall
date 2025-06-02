035c754e73da29be6ffd3bc83870c9fb1fc0d6f5
Erfan Mola <hi@erfanxd.ir>
2025-06-02 19:42:33 +0330

    Implement Root Layout

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,11 +1,22 @@
-import { Outlet, ScrollRestoration } from "react-router";
+import { Outlet, ScrollRestoration, useLocation } from "react-router";
+
+import BottomBar from "../components/BottomBar";
+import { useMemo } from "react";
 
 const LayoutRoot = () => {
+	const location = useLocation();
+	const displayBottombar = useMemo(
+		() => ["/", "/profile"].includes(location.pathname),
+		[location.pathname],
+	);
+
 	return (
 		<>
 			<main>
 				<Outlet />
 			</main>
+
+			{displayBottombar && <BottomBar />}
 
 			<ScrollRestoration />
 		</>
