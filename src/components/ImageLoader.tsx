f6dea513eb4689bbc07d5e9206e01423ed35bb2c
Erfan Mola <hi@erfanxd.ir>
2025-06-02 19:43:14 +0330

    Implement Image Loader Component

---
+++ src/components/ImageLoader.tsx
@@ -0,0 +1,31 @@
+import "./ImageLoader.scss";
+import { useEffect, useRef, useState, type FC } from "react";
+import { ShimmerThumbnail } from "react-shimmer-effects";
+
+const ImageLoader: FC<{ src: string }> = ({ src }) => {
+	const imgRef = useRef<HTMLImageElement>(null);
+	const [loaded, setLoaded] = useState<boolean | null>(null);
+
+	useEffect(() => {
+		const img = imgRef.current;
+		if (!img) return;
+		setLoaded(img.complete);
+
+		const handleLoad = () => setLoaded(true);
+
+		img.addEventListener("load", handleLoad);
+
+		return () => {
+			img.removeEventListener("load", handleLoad);
+		};
+	}, [src]);
+
+	return (
+		<div className="image-loader">
+			{loaded === false && <ShimmerThumbnail />}
+			<img ref={imgRef} src={src} />
+		</div>
+	);
+};
+
+export default ImageLoader;
