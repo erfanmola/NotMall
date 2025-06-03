b4a617dddd6ecae6134954817f334eefd6e7ada6
Erfan Mola <hi@erfanxd.ir>
2025-06-03 18:53:07 +0330

    Chore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,63 @@ import "./Home.scss";
 
 import { IconBasket, IconSearch } from "../components/Icons";
 
-import type { FC } from "react";
-import type { Item } from "../stores/useItemsStore";
-import { useLoadItems } from "../hooks/useLoadItems";
+import { useEffect, useRef, useState, type FC } from "react";
+import { useItemsStore, type Item } from "../stores/useItemsStore";
 import { useTranslation } from "react-i18next";
 import ImageLoader from "../components/ImageLoader";
 import { Link } from "react-router";
 import { ShimmerThumbnail, ShimmerTitle } from "react-shimmer-effects";
+import { invokeHapticFeedbackImpact } from "../utils/telegram";
+import { VirtuosoGrid } from "react-virtuoso";
+import ReactSimplyCarousel from "react-simply-carousel";
 
 const Item: FC<{ item: Item }> = ({ item }) => {
+	const headerRef = useRef<HTMLElement>(null);
+	const [headerSize, setHeaderSize] = useState(0);
+	const [activeSlide, setActiveSlide] = useState(
+		Math.floor(Math.random() * item.images.length),
+	);
+
+	useEffect(() => {
+		if (!headerRef.current) return;
+		setHeaderSize(headerRef.current.clientWidth);
+	}, [headerRef]);
+
 	return (
 		<Link to={"/"} className="container-item-home">
-			<header>
-				<ImageLoader src={item.images[0]} />
+			<header ref={headerRef}>
+				{headerSize && (
+					<>
+						<ReactSimplyCarousel
+							activeSlideIndex={activeSlide}
+							onRequestChange={setActiveSlide}
+							itemsToShow={1}
+							itemsToScroll={1}
+							speed={250}
+							easing="linear"
+							centerMode
+							infinite
+						>
+							{item.images.map((image, index) => (
+								<div
+									key={index}
+									style={{ width: headerSize, height: headerSize }}
+								>
+									<ImageLoader src={image} />
+								</div>
+							))}
+						</ReactSimplyCarousel>
+
+						<ul>
+							{item.images.map((_, index) => (
+								<li
+									key={index}
+									className={index === activeSlide ? "active" : ""}
+								></li>
+							))}
+						</ul>
+					</>
+				)}
 			</header>
 
 			<div>
@@ -28,20 +72,47 @@ const Item: FC<{ item: Item }> = ({ item }) => {
 };
 
 const Items: FC<{ items: Item[] }> = ({ items }) => {
-	return items.map((item) => <Item item={item} key={item.id} />);
+	if (items.length <= 12) {
+		return (
+			<div className="container-grid-items-home">
+				<div className="container-items-home">
+					{items.map((item, index) => (
+						<Item item={item} key={index} />
+					))}
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<VirtuosoGrid
+				totalCount={items.length}
+				overscan={{
+					main: 2,
+					reverse: 2,
+				}}
+				className="container-grid-items-home"
+				listClassName="container-items-home"
+				itemContent={(index) => <Item item={items[index]} />}
+			/>
+		);
+	}
 };
 
 const ItemsSkeleton = () => {
-	return Array.from(new Array(6)).map((_, index) => (
-		<div className="container-item-shimmer-home" key={index}>
-			<ShimmerThumbnail />
+	return (
+		<div id="container-items-shimmer-home">
+			{Array.from(new Array(6)).map((_, index) => (
+				<div className="container-item-shimmer-home" key={index}>
+					<ShimmerThumbnail />
 
-			<div>
-				<ShimmerTitle line={1} variant="primary" />
-				<ShimmerTitle line={1} variant="secondary" />
-			</div>
+					<div>
+						<ShimmerTitle line={1} variant="primary" />
+						<ShimmerTitle line={1} variant="secondary" />
+					</div>
+				</div>
+			))}
 		</div>
-	));
+	);
 };
 
 const ItemsError = () => {
@@ -51,7 +122,15 @@ const ItemsError = () => {
 
 const PageHome = () => {
 	const { t } = useTranslation();
-	const { items, loading } = useLoadItems();
+	const { items, loading, fetchItems } = useItemsStore();
+
+	useEffect(() => {
+		if (!items) {
+			fetchItems();
+		}
+
+		invokeHapticFeedbackImpact("medium");
+	}, []);
 
 	return (
 		<div id="container-page-home">
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,15 @@
 import "./Profile.scss";
 
+import { invokeHapticFeedbackImpact } from "../utils/telegram";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const PageProfile = () => {
 	const { t } = useTranslation();
+
+	useEffect(() => {
+		invokeHapticFeedbackImpact("medium");
+	}, []);
 
 	return (
 		<div id="container-page-profile">
