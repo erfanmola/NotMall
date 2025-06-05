0688f92c081388d5d2c7922babbbc092bec8f6aa
Erfan Mola <hi@erfanxd.ir>
2025-06-05 23:06:15 +0330

    Implement Lottie Animations

---
+++ src/components/LottiePlayer.tsx
@@ -0,0 +1,49 @@
+import Lottie from "lottie-light-react";
+import { useEffect, useState, type FC, type JSX } from "react";
+
+export type LottiePlayerProps = {
+	src: string;
+	autoplay?: boolean;
+	loop?: boolean;
+	fallback?: JSX.Element;
+};
+
+export const LottiePlayerFileCache: { [key: string]: any } = {};
+
+const LottiePlayer: FC<LottiePlayerProps> = (props) => {
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		if (props.src in LottiePlayerFileCache) {
+			setLoaded(true);
+		} else {
+			fetch(props.src).then(async (response) => {
+				try {
+					LottiePlayerFileCache[props.src] = JSON.parse(await response.text());
+					setLoaded(true);
+				} catch (error) {
+					console.error("Failed to load animation", error);
+					setLoaded(false);
+				}
+			});
+		}
+	}, [props.src]);
+
+	return (
+		<div className="lottie-animation">
+			{loaded && (
+				<Lottie
+					animationData={LottiePlayerFileCache[props.src]}
+					loop={props.loop ?? false}
+					autoplay={props.autoplay ?? false}
+				/>
+			)}
+			{!loaded &&
+				props.fallback &&
+				!(props.src in LottiePlayerFileCache) &&
+				props.fallback}
+		</div>
+	);
+};
+
+export default LottiePlayer;
---
+++ src/utils/lottie.ts
@@ -0,0 +1,14 @@
+export const lottieAnimations = {
+	chick: {
+		url: "/assets/lottie/chick.json",
+		emoji: "🐣",
+	},
+	confetti: {
+		url: "/assets/lottie/confetti.json",
+		emoji: "🎉",
+	},
+	duckForbidden: {
+		url: "/assets/lottie/duck-forbidden.json",
+		emoji: "🚫",
+	},
+};
