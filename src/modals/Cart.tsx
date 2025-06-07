fa539c25cee670e8cf290ec76dec137b95f98481
Erfan Mola <hi@erfanxd.ir>
2025-06-07 15:25:07 +0330

    Migrate to Vaul

--- src/modals/Cart.tsx
+++ src/modals/Cart.tsx
@@ -1,13 +1,13 @@
-import type { Dispatch, FC, SetStateAction } from "react";
+import { useMemo, type Dispatch, type FC, type SetStateAction } from "react";
 
 import { FaMinus } from "react-icons/fa6";
 import ImageLoader from "../components/ImageLoader";
 import { IoClose } from "react-icons/io5";
-import { Sheet } from "react-modal-sheet";
 import { invokeHapticFeedbackImpact } from "../utils/telegram";
 import { useCartStore } from "../stores/useCartStore";
 import { useItemsStore } from "../stores/useItemsStore";
 import { useTranslation } from "react-i18next";
+import { Drawer } from "vaul";
 
 const ModalCart: FC<{
 	isOpen: boolean;
@@ -17,102 +17,112 @@ const ModalCart: FC<{
 	const { t } = useTranslation();
 	const { items } = useItemsStore();
 
-	return (
-		<Sheet
-			isOpen={isOpen}
-			onClose={() => setOpen(false)}
-			detent="content-height"
-		>
-			<Sheet.Container>
-				{Object.keys(cart).length > 0 ? (
-					<Sheet.Content className="container-modal-cart">
-						<span
-							className="btn-close-modal"
-							onClick={() => {
-								invokeHapticFeedbackImpact("light");
-								setOpen(false);
-							}}
-						>
-							<IoClose />
-						</span>
+	const renderContent = useMemo(() => {
+		if (Object.keys(cart).length > 0) {
+			return (
+				<div className="container-modal-cart">
+					<span
+						className="btn-close-modal"
+						onClick={() => {
+							invokeHapticFeedbackImpact("light");
+							setOpen(false);
+						}}
+					>
+						<IoClose />
+					</span>
+
+					<div>
+						<header>
+							<h2>{t("modals.cart.title")}</h2>
+						</header>
 
 						<div>
-							<header>
-								<h2>{t("modals.cart.title")}</h2>
-							</header>
+							{Object.entries(cart).map(([productId, quantity]) => {
+								const item = items?.find(
+									(i) => i.id === Number.parseInt(productId),
+								);
+								if (!item) return;
 
-							<div>
-								{Object.entries(cart).map(([productId, quantity]) => {
-									const item = items?.find(
-										(i) => i.id === Number.parseInt(productId),
-									);
-									if (!item) return;
+								return (
+									<div key={productId}>
+										<ImageLoader src={item.images[0]} />
 
-									return (
 										<div>
-											<ImageLoader src={item.images[0]} />
-
-											<div>
-												<span>{item.category}</span>
-												<h3>{item.name}</h3>
-											</div>
-
-											<span>
-												{quantity > 1 && (
-													<span>
-														{quantity} <IoClose />
-													</span>
-												)}
-												{item.price.toLocaleString()} {item.currency}
-											</span>
-
-											<span
-												onClick={() => {
-													invokeHapticFeedbackImpact("medium");
-													remove(item.id.toString());
-												}}
-											>
-												<FaMinus />
-											</span>
+											<span>{item.category}</span>
+											<h3>{item.name}</h3>
 										</div>
-									);
-								})}
-							</div>
-						</div>
-					</Sheet.Content>
-				) : (
-					<Sheet.Content className="container-modal-cart-empty">
-						<span
-							className="btn-close-modal"
-							onClick={() => {
-								invokeHapticFeedbackImpact("light");
-								setOpen(false);
-							}}
-						>
-							<IoClose />
-						</span>
 
-						<div>
-							<h2>{t("modals.cart.noItems.title")}</h2>
-							<span>{t("modals.cart.noItems.description")}</span>
-						</div>
+										<span>
+											{quantity > 1 && (
+												<span>
+													{quantity} <IoClose />
+												</span>
+											)}
+											{item.price.toLocaleString()} {item.currency}
+										</span>
 
-						<div id="container-action-buttons" style={{ paddingTop: "0" }}>
-							<div
-								className="primary"
-								onClick={() => {
-									invokeHapticFeedbackImpact("light");
-									setOpen(false);
-								}}
-							>
-								<span>{t("general.ok")}</span>
-							</div>
+										<span
+											onClick={() => {
+												invokeHapticFeedbackImpact("medium");
+												remove(item.id.toString());
+											}}
+										>
+											<FaMinus />
+										</span>
+									</div>
+								);
+							})}
 						</div>
-					</Sheet.Content>
-				)}
-			</Sheet.Container>
-			<Sheet.Backdrop onTap={() => setOpen(false)} />
-		</Sheet>
+					</div>
+				</div>
+			);
+		}
+
+		return (
+			<div className="container-modal-cart-empty">
+				<span
+					className="btn-close-modal"
+					onClick={() => {
+						invokeHapticFeedbackImpact("light");
+						setOpen(false);
+					}}
+				>
+					<IoClose />
+				</span>
+
+				<div>
+					<h2>{t("modals.cart.noItems.title")}</h2>
+					<span>{t("modals.cart.noItems.description")}</span>
+				</div>
+
+				<div id="container-action-buttons" style={{ paddingTop: "0" }}>
+					<div
+						className="primary"
+						onClick={() => {
+							invokeHapticFeedbackImpact("light");
+							setOpen(false);
+						}}
+					>
+						<span>{t("general.ok")}</span>
+					</div>
+				</div>
+			</div>
+		);
+	}, [cart, items]);
+
+	return (
+		<Drawer.Root open={isOpen} onOpenChange={setOpen}>
+			<Drawer.Portal>
+				<Drawer.Overlay className="vaul-overlay" />
+				<Drawer.Content className="vaul-content" aria-describedby={undefined}>
+					<Drawer.Title style={{ display: "none" }}>
+						{t("modals.cart.title")}
+					</Drawer.Title>
+
+					<div>{renderContent}</div>
+				</Drawer.Content>
+			</Drawer.Portal>
+		</Drawer.Root>
 	);
 };
 
